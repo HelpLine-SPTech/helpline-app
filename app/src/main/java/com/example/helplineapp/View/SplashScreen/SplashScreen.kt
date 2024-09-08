c1e4484312a7b3470caa466be13ab7a8fe6edba0
GuiScarabelli <guilherme.quadros@sptech.school>
2024-09-08 17:51:23 -0300

    Adding a BackHandler to avoid crash

--- app/src/main/java/com/example/helplineapp/View/SplashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/helplineapp/View/SplashScreen/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.helplineapp.View.SplashScreen
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -12,10 +13,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,6 +37,9 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+
+  BackHandler {}
+
   //Controla o tempo de exibição da splash screen (2 segundos neste caso)
   LaunchedEffect(key1 = true) {
     delay(2000)
@@ -68,15 +78,5 @@ fun SplashScreen(navController: NavController) {
           .aspectRatio(1.43f)
       )
     }
-  }
-}
-
-@Composable
-fun MainScreen() {
-  Box(
-    modifier = Modifier.fillMaxSize(),
-    contentAlignment = Alignment.Center
-  ) {
-    Text(text = "Bem-vindo ao App!", fontSize = 24.sp)
   }
 }
